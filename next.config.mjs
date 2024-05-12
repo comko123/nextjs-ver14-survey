ecb94da7bedc341defdc8971abcae49d98da42cf
Minseok Kim <comko123@naver.com>
2024-05-13 00:03:31 +0900

    set : 부가 설정및 기타 패키지 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    logging: {
+        fetches: {
+            fullUrl: true
+        }
+    },
+    experimental: {
+        taint: true
+    },
+    images: {
+        remotePatterns: []
+    }
+};
 
 export default nextConfig;
