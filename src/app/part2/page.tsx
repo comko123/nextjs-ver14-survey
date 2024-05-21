a72bc795b779cc441824659f4924439cf4e3bc57
Minseok Kim <comko123@naver.com>
2024-05-22 00:20:54 +0900

    .

--- src/app/part2/page.tsx
+++ src/app/part2/page.tsx
@@ -2,6 +2,27 @@ function Part2() {
   return (
     <main>
       <h1>certified form</h1>
+      <form>
+        <article className="flex mb-5 flex-col">
+          <section className="flex gap-x-2">
+            <input type="email" name="email" className="bg-rose-100" />
+            <input type="button" value="email" />
+          </section>
+          <section>
+            <input type="number" className="bg-rose-100" />
+          </section>
+        </article>
+        <article className="flex mb-5 flex-col">
+          <section className="flex gap-x-2">
+            <input type="number" name="phone" className="bg-rose-100" />
+            <input type="button" value="phone" />
+          </section>
+          <section>
+            <input type="number" className="bg-rose-100" />
+          </section>
+        </article>
+        <input type="submit" value="submit" />
+      </form>
     </main>
   )
 }
