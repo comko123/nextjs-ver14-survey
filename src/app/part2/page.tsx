8a77cb3a65be52e3a6e0be3e28ff9f09b913edd9
Minseok Kim <comko123@naver.com>
2024-05-26 00:22:00 +0900

    etc : mark up added and etc....

--- src/app/part2/page.tsx
+++ src/app/part2/page.tsx
@@ -1,32 +1,19 @@
+import CertifiedForm from "@/components/part2/certifiedForm"
+
 function Part2() {
   return (
     <main className="form-page-layout">
       <h1 className="form-page-title">certified form</h1>
-      <form>
-        <article className="flex mb-5 flex-col gap-y-4">
-          <section className="flex gap-x-2">
-            <input type="email" name="email" className="bg-rose-100" />
-            <input type="button" value="email" className="gradient-btn2" />
-          </section>
-          <section>
-            <input type="number" className="bg-rose-100" />
-          </section>
-        </article>
-        <article className="flex mb-5 flex-col gap-y-4">
-          <section className="flex gap-x-2">
-            <input type="number" name="phone" className="bg-rose-100" />
-            <input type="button" value="phone" className="gradient-btn2" />
-          </section>
-          <section>
-            <input type="number" className="bg-rose-100" />
-          </section>
-        </article>
-        <input type="submit" value="submit" className="gradient-btn2" />
-      </form>
+      <CertifiedForm />
     </main>
   )
 }
 
 /*peer , has , group 생성자 사용하기 */
-
+/*
+peer and group의 차이점
+peer - 한단계 위의 부모 html엘리먼트가 깉은 html태그안에서 사용 가능
+group - 같은 부모 html엘리먼트안에서 사용 가능
+주의점 gradiant를 사용하는 경우 peer나 group 에서도 같이 gradiant를사용해야함 
+*/
 export default Part2
---
+++ src/types/certifiedForm.d.ts
@@ -0,0 +1,8 @@
+interface Auth {
+  title: string
+  [key: string]: boolean | string
+}
+
+interface Certified_State {
+  [key: string]: boolean
+}
